7acb72ae67128c6a536e0ce7e88986b5008573c7
Raphael Azevedo Magalhães Carvalho <raphaelazevedocarvalho@gmail.com>
2016-08-24 19:39:42 -0300

    no message

--- enviar_contato.php
+++ enviar_contato.php
@@ -4,8 +4,7 @@
 
 // Recebendo os dados passados pela página "form_contato.php"
 
-// Vamos definir agora o destinatário do email, ou seja, você ou seu cliente
-
+// Vamos definir agora o destinatário do email
 $para = "raphaelazevedocarvalho@gmail.com";
 
 // Definindo o aspecto da mensagem
@@ -16,12 +15,17 @@ $texto_final .=  "Email: {$_POST['email']}<br>";
 $texto_final .=  "Telefone: {$_POST['telefone']}<br>";
 $texto_final .=  "Celular: {$_POST['celular']}<br>";
 
-
 $headers = "Content-type:text/html; charset=utf-8"; 
 
 // Enviando a mensagem para o destinatário
 $envia = mail($para,"E-mail do Site",$texto_final,$headers);
 
+
+//email de agradecimento (enviar para o cliente)
+
+$agradecimento = file_get_contents("agradecimento.php");
+
+$envia = mail({$_POST['email']},"Agradecimento do site",$agradecimento,$headers);
 
 header('location: agradecimento.php');
 
